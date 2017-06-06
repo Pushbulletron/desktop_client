6d9f011f5dfe3ee77297dd4ef1e60b50acbd6f7b
Bryan Kok <bryan.wyern1@gmail.com>
2017-06-06 20:54:05 +0800

    Added spectron to package.json
    Sample testing code

---
+++ test/test.js
@@ -0,0 +1,11 @@
+var assert = require('assert');
+
+describe('plus_behavior', function() {
+	it('should equals 10', function() {
+		assert.equal(10, 5*2);
+	});
+
+	it('should equals 2', function() {
+		assert.equal(2, 1+1);
+	});
+});
